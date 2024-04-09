71f0ff16289bf9d46e2191fe0bb42d878c881313
giangnt <giangnt@tecapro.com.vn>
2024-04-09 22:26:35 +0700

    update

---
+++ src/main/java/vn/com/gsoft/inventory/service/PhieuThuChisService.java
@@ -0,0 +1,7 @@
+package vn.com.gsoft.inventory.service;
+
+import vn.com.gsoft.inventory.entity.PhieuThuChis;
+import vn.com.gsoft.inventory.model.dto.PhieuThuChisReq;
+
+public interface PhieuThuChisService extends BaseService<PhieuThuChis, PhieuThuChisReq, Long>{
+}
