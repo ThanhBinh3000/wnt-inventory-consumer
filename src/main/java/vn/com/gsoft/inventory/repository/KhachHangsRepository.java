df73be6df5df7e7bcc948d60937748ded452b89a
giangnt <giangnt@tecapro.com.vn>
2024-04-16 11:20:25 +0700

    update

--- src/main/java/vn/com/gsoft/inventory/repository/KhachHangsRepository.java
+++ src/main/java/vn/com/gsoft/inventory/repository/KhachHangsRepository.java
@@ -1,6 +1,7 @@
 package vn.com.gsoft.inventory.repository;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import vn.com.gsoft.inventory.entity.KhachHangs;
 import vn.com.gsoft.inventory.model.dto.KhachHangsReq;
@@ -8,7 +9,7 @@ import vn.com.gsoft.inventory.model.dto.KhachHangsReq;
 import java.util.Optional;
 
 @Repository
-public interface KhachHangsRepository extends BaseRepository<KhachHangs, KhachHangsReq, Long> {
+public interface KhachHangsRepository extends CrudRepository<KhachHangs, Long> {
     @Query("SELECT kh FROM KhachHangs kh where kh.customerTypeId=1 and kh.maNhaThuoc= ?1 ")
     Optional<KhachHangs> findKhachHangLe(String storeCode);
 }
