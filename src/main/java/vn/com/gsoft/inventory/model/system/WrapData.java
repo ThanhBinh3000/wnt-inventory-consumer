82c39c52833b2a67fb3b4e8677e029f8bb76831c
giangnt <giangnt@tecapro.com.vn>
2024-04-18 15:40:58 +0700

    update

--- src/main/java/vn/com/gsoft/inventory/model/system/WrapData.java
+++ src/main/java/vn/com/gsoft/inventory/model/system/WrapData.java
@@ -2,8 +2,11 @@ package vn.com.gsoft.inventory.model.system;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class WrapData {
     private String code;
+    private Date sendDate;
     private Object data;
 }
