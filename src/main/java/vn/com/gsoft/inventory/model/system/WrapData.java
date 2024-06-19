7370cfa2856ce6eb62345a258dd71b4a1ad5cf50
giangnt <giangnguyen9699@gmail.com>
2024-06-19 11:30:45 +0700

    update

--- src/main/java/vn/com/gsoft/inventory/model/system/WrapData.java
+++ src/main/java/vn/com/gsoft/inventory/model/system/WrapData.java
@@ -6,7 +6,10 @@ import java.util.Date;
 
 @Data
 public class WrapData {
+    private String bathKey;
     private String code;
     private Date sendDate;
     private Object data;
+    private Integer index;
+    private Integer total;
 }
